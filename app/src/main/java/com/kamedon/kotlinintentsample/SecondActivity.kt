a3ede5f0c2dce17c6b4d89151857c50ca18bb041
Kamei Hidetoshi <h_kamei@menue.co.jp>
2016-04-22 11:08:49 +0900

    2つ目のアクティビティを作成

---
+++ app/src/main/java/com/kamedon/kotlinintentsample/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.kamedon.kotlinintentsample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by h_kamei on 2016/04/22.
+ */
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
