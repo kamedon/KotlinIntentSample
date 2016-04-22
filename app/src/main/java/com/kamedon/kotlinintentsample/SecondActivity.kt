053fb13525baccd6cc3aedc1059f7de5f077f7c1
Kamei Hidetoshi <h_kamei@menue.co.jp>
2016-04-22 11:12:34 +0900

    ui作成

--- app/src/main/java/com/kamedon/kotlinintentsample/SecondActivity.kt
+++ app/src/main/java/com/kamedon/kotlinintentsample/SecondActivity.kt
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
     }
 
 
---
+++ app/src/main/java/com/kamedon/kotlinintentsample/page/Page.kt
@@ -0,0 +1,9 @@
+package com.kamedon.kotlinintentsample.page
+
+/**
+ * Created by h_kamei on 2016/04/22.
+ */
+enum class Page {
+    MAIN, SECOND;
+
+}
